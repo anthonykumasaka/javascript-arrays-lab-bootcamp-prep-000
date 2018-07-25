a54585848498ab87f61c3077c0eb4c450d2258bd
anthonykumasaka@gmail.com Kumasaka <anthonykumasaka@gmail.com>
2018-07-25 18:30:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Add your functions and code here
+// // Add your functions and code here
+//   describe('destructivelyAppendKitten(name)', function() {
+//     it('appends a kitten to the end of the kittens array', function() {
+//       destructivelyAppendKitten('Ralph')
+//       expect(window.kittens).toEqual(["Milo", "Otis", "Garfield", "Ralph"])
+//     })
+//   })
+  
+  function destructivelyAppendKitten(name){
+    kittens.push(name)
+    return kittens; 
+  }
+  
+  function destructivelyPrependKitten(name){
+    kittens.unshift(name)
+    return kittens;
+  }
+  
+  function destructivelyRemoveLastKitten(){
+    kittens.pop();
+    return kittens; 
+  }
+  
+  function destructivelyRemoveFirstKitten() {
+    kittens.shift();
+    return kittens; 
+  }
+  
+  function appendKitten(name) {
+    return [...kittens, name]; 
+  }
+  
+  function prependKitten(name){
+    return [name, ...kittens];
+  }
+  
+  function removeLastKitten(){
+    kittens.splice((kittens.length), 1)
+    return kittens; 
+  }
